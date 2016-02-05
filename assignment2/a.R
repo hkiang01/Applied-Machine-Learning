a4c99bf4bc4d80807548d006d524277ae557017a
hkiang01 <hkiang01@yahoo.com>
2016-02-05 01:03:38 -0600

    1 and -1

--- assignment2/a.R
+++ assignment2/a.R
@@ -3,13 +3,30 @@ library(klaR)
 library(caret)
 bigx<-wdat[c(1,3,5,11,12,13)] # continuous features
 bigy<-wdat[,15] # labels
-tr_indices<-createDataPartition(y=bigy, p=.8, list=FALSE)
-trdat<-bigx[tr_indices,]
-trlab<-bigy[tr_indices]
+bigy2<-matrix() #blank matrix
+
+# for loop to replace <=50K with -1
+# and >50K with 1
+counter<-0
+for(i in bigy) {
+  #if(identical(j, i)) {
+  if(identical(" <=50K", i)) {
+    bigy2[counter]<- -1
+  }
+  else {
+    bigy2[counter]<- 1
+  }
+  counter<-counter+1
+}
+
+tr_indices<-createDataPartition(y=bigy2, p=.8, list=FALSE)
+trdat<-bigx[tr_indices,] # training features
+trlab<-bigy2[tr_indices] #training labels
 otherdat<-bigx[-tr_indices,]
-otherlab<-bigy[-tr_indices]
+otherlab<-bigy2[-tr_indices]
 test_indices<-createDataPartition(y=otherlab, p=.5, list=FALSE)
-testdat<-otherdat[test_indices,]
-testlab<-otherlab[test_indices]
-valdat<-otherdat[-test_indices,]
-vallab<-otherlab[-test_indices]
+testdat<-otherdat[test_indices,] # test features
+testlab<-otherlab[test_indices] # test labels
+valdat<-otherdat[-test_indices,] #validation features
+vallab<-otherlab[-test_indices] #validation labels
+ 
