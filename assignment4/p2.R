0dd36fd6b1834029c1eaeeb2ef14d90b7b57af17
hkiang01 <hkiang01@yahoo.com>
2016-02-27 16:49:58 -0600

    covmatrix

--- assignment4/p2.R
+++ assignment4/p2.R
@@ -1,5 +1,7 @@
 # 3.4.2 for 2-16 version of notes for general procedure
 # see procedure 3.1
+# see definition 3.1 for covariance
+# see definition 3.2 and for covariance matrix (section 3.2, page 63 of 2/16/16 version of notes)
 
 setwd('/Users/harry/projects/aml/assignment4/')
 wdat<-read.csv('wine_data.csv', header=FALSE)
@@ -10,4 +12,18 @@ bigy<-wdat[,1]
 # each component of mean{x} is the mean of that component of the data
 # see 3.2.1 The Mean
 # http://stackoverflow.com/questions/18065929/how-to-row-wise-subtract-a-vector-keeping-the-means-of-a-data-frames-df-column
-bigx<-scale(bigx, center=TRUE, scale=FALSE)
+bigx2<-scale(bigx, center=TRUE, scale=FALSE)
+bigx3<-t(bigx2)
+covmat<-matrix(data=0, nrow=NCOL(bigx3), ncol=NCOL(bigx3)) #the covmat
+
+# populate the covmat
+for (j in 1:NCOL(bigx3)) {
+  for (k in 1:NCOL(bigx3)) {
+    x_j<-as.matrix(bigx3[,c(j)])
+    x_k<-as.matrix(t(bigx3[,c(k)]))
+    a<-(x_j*t(x_k))
+    b<-sum(a)
+    covmat[,c(j)][k]<-b
+  }
+}
+
