2f52efb2fb6e25f411c907eef9bbef93bbfc8534
hkiang01 <hkiang01@yahoo.com>
2016-02-27 18:06:47 -0600

    plot complete

--- assignment4/p2.R
+++ assignment4/p2.R
@@ -4,6 +4,7 @@
 # see definition 3.2 and for covariance matrix (section 3.2, page 63 of 2/16/16 version of notes)
 
 setwd('/Users/harry/projects/aml/assignment4/')
+library('mixOmics')
 wdat<-read.csv('wine_data.csv', header=FALSE)
 bigx<-wdat[,-c(1)] #label is the first col
 bigy<-wdat[,1]
@@ -12,22 +13,33 @@ bigy<-wdat[,1]
 # each component of mean{x} is the mean of that component of the data
 # see 3.2.1 The Mean
 # http://stackoverflow.com/questions/18065929/how-to-row-wise-subtract-a-vector-keeping-the-means-of-a-data-frames-df-column
-bigx2<-scale(bigx, center=TRUE, scale=FALSE)
-bigx3<-t(bigx2)
-covmat<-matrix(data=0, nrow=NCOL(bigx3), ncol=NCOL(bigx3)) #the covmat
+bigx2<-scale(bigx, center=TRUE, scale=TRUE)
+# BUGGY CODE
+# bigx3<-t(bigx2)
+# covmat<-matrix(data=0, nrow=NCOL(bigx3), ncol=NCOL(bigx3)) #the covmat
+# 
+# # populate the covmat
+# for (j in 1:NCOL(bigx3)) {
+#   for (k in 1:NCOL(bigx3)) {
+#     x_j<-as.matrix(bigx3[,c(j)])
+#     x_k<-as.matrix(t(bigx3[,c(k)]))
+#     val<-sum((x_j*t(x_k)))
+#     covmat[,c(j)][k]<-val
+#   }
+# }
+# 
+# eigendat<-eigen(covmat)
+# eigenvalues<-eigendat$values
+# eigenvalues_sorted<-sort(eigenvalues)
+# plot(eigenvalues_sorted)
+# END OF BUGGY CODE
 
-# populate the covmat
-for (j in 1:NCOL(bigx3)) {
-  for (k in 1:NCOL(bigx3)) {
-    x_j<-as.matrix(bigx3[,c(j)])
-    x_k<-as.matrix(t(bigx3[,c(k)]))
-    val<-sum((x_j*t(x_k)))
-    val<-val/NCOL(bigx3)
-    covmat[,c(j)][k]<-val
-  }
-}
+# PART A
+x_nipals<-nipals(bigx2, ncomp=10) #the first eigenvaue is much larger
+eigvalues<-x_nipals$eig
+plot(eigvalues)
+# 4-7 components are good
+# based on visual analysis as on bottom of page 81 in 2/16 version of notes
+# END OF PART A
 
-eigendat<-eigen(covmat)
-eigenvalues<-eigendat$values
-eigenvalues_sorted<-sort(eigenvalues)
-plot(eigenvalues_sorted)
+# PART B
