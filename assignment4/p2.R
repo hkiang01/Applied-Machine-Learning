250d426811816a10a4bc49f512dd5759d6bc9313
hkiang01 <hkiang01@yahoo.com>
2016-02-28 21:07:11 -0600

    plotting done, only part c left

--- assignment4/p2.R
+++ assignment4/p2.R
@@ -1,38 +1,9 @@
-# 3.4.2 for 2-16 version of notes for general procedure
-# see procedure 3.1
-# see definition 3.1 for covariance
-# see definition 3.2 and for covariance matrix (section 3.2, page 63 of 2/16/16 version of notes)
-
 setwd('/Users/harry/projects/aml/assignment4/')
 library('mixOmics')
 wdat<-read.csv('wine_data.csv', header=FALSE)
 bigx<-wdat[,-c(1)] #label is the first col
 bigy<-wdat[,1]
-
-# translate the blob so its mean is at the origin
-# each component of mean{x} is the mean of that component of the data
-# see 3.2.1 The Mean
-# http://stackoverflow.com/questions/18065929/how-to-row-wise-subtract-a-vector-keeping-the-means-of-a-data-frames-df-column
 bigx2<-scale(bigx, center=TRUE, scale=TRUE)
-# BUGGY CODE
-# bigx3<-t(bigx2)
-# covmat<-matrix(data=0, nrow=NCOL(bigx3), ncol=NCOL(bigx3)) #the covmat
-# 
-# # populate the covmat
-# for (j in 1:NCOL(bigx3)) {
-#   for (k in 1:NCOL(bigx3)) {
-#     x_j<-as.matrix(bigx3[,c(j)])
-#     x_k<-as.matrix(t(bigx3[,c(k)]))
-#     val<-sum((x_j*t(x_k)))
-#     covmat[,c(j)][k]<-val
-#   }
-# }
-# 
-# eigendat<-eigen(covmat)
-# eigenvalues<-eigendat$values
-# eigenvalues_sorted<-sort(eigenvalues)
-# plot(eigenvalues_sorted)
-# END OF BUGGY CODE
 
 # PART A
 x_nipals<-nipals(bigx2, ncomp=10) #the first eigenvaue is much larger
@@ -40,6 +11,29 @@ eigvalues<-x_nipals$eig
 plot(eigvalues)
 # 4-7 components are good
 # based on visual analysis as on bottom of page 81 in 2/16 version of notes
-# END OF PART A
 
 # PART B
+# stem plotting function from: http://www.r-bloggers.com/matlab-style-stem-plot-with-r/
+# created by Matti Pastell
+attrib_labels<-c("Alcohol", "Malic acid", "Ash", "Alcalinity of ash",
+                 "Magnesium", "Total phenols", "Flavanoids",
+                 "Nonflavanoid phenols", "Color intensity", "Hue",
+                 "OD280/OD315 of diluted wines", "Proline")
+stem <- function(x,y,pch=16,linecol=1,clinecol=1,eig_iter=0,...){
+  if (missing(y)){
+    y = x
+    x = 1:length(x) }
+  plot(x,y,main=paste("Wine Recognition: Stem Plot of Principal Component",toString(eig_iter)),
+       xlab="Attributes",ylab="Energy",
+       pch=pch,text(x,y*.8-y*.3*(x%%2)-y*.1*(x%%3),attrib_labels),...)
+  for (i in 1:length(x)){
+    lines(c(x[i],x[i]), c(0,y[i]),col=linecol)
+  }
+  lines(c(x[1]-2,x[length(x)]+2), c(0,0),col=clinecol)
+}
+
+x_nipals_selected<-nipals(bigx2, ncomp=3)
+eigvectors<-x_nipals_selected$p
+for(i in 1:NCOL(eigvectors)) {
+  stem(eigvectors[,i],eig_iter=i)
+}
