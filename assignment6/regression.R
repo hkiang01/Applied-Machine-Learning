a3a4eac8c6dc03a64d101917707b58f43dc30f74
annlinsheih <sheih2@illinois.edu>
2016-04-04 00:35:26 -0500

    got R^2 values

--- assignment6/regression.R
+++ assignment6/regression.R
@@ -1,12 +1,16 @@
 library('caret') #createDataPartition
 library('stats') #lm
+library('MASS')  #boxcox
 
 #You should ignore outliers.
 #You should regard latitude and longitude as entirely independent.
 
 #last 2 cols are lat and long
 
-setwd('/Users/harry/projects/aml/assignment6/')
+# setwd('/Users/harry/projects/aml/assignment6/') # harrison's
+setwd('/Users/annlinsheih/Dev/aml/assignment6') # annlin's
+# source('~/Dev/aml/assignment6/regression.R')
+
 rawdat<-read.csv('default_plus_chromatic_features_1059_tracks.txt', header=FALSE)
 xdat<-rawdat[,-c(ncol(rawdat),ncol(rawdat)-1)]
 ydat<-rawdat[,c(ncol(rawdat)-1,ncol(rawdat))]
@@ -20,6 +24,7 @@ y_long<-ydat[,c(2)]
 #What is the R-squared? Plot a graph evaluating each regression.
 # y = t(x)*β + ξ, where β is the vector of weights, ξ is random with 0 mean (unmodelled)
 #Goal: estimate β
+
 
 #LATITUDE
 
@@ -94,3 +99,44 @@ plot(x=predicted_long, y=residuals_long, type="p",
 # }
 
 long_r_sqared_val<-summary(foo_long.lm)$r.squared
+long_r_squared_val_adj<-summary(foo_long.lm)$adj.r.squared
+
+##################################################
+################### BOX-COX ######################
+##################################################
+
+# First I did Box-Cox on Y_origin, to get a Y_boxed. 
+y_lat_offset <- y_lat_obj + 90
+y_long_offset <- y_long_obj + 90
+# predicted_lat_offset <- predicted_lat + predicted_lat_min+1e-5
+# predicted_long_offset <- predicted_long + predicted_long_min+1e-5
+
+# y vs xdat
+bc_lat<-boxcox(y_lat_offset~as.matrix(xdat), lambda = seq(-5, 5, 1/10))
+title("Y Lat. Offset vs. Xdat Obj.")
+bc_long<-boxcox(y_long_offset~as.matrix(xdat), lambda = seq(-5, 5, 1/10))
+title("Y Long. Offset vs. Xdat Obj.")
+lambda_lat<-bc_lat$x[which.max(bc_lat$y)]
+lambda_long<-bc_long$x[which.max(bc_long$y)]
+
+y_lat_boxed<-(y_lat_offset^lambda_lat - 1)/lambda_lat
+#y_lat_boxed[is.na(y_lat_boxed)]<-0
+y_long_boxed<-(y_long_offset^lambda_long - 1)/lambda_long
+
+# Then I did linear regression with X and Y_boxed.
+# linear regression
+reg_bc_lat<-data.frame(ind=xdat, dep=y_lat_boxed)
+reg_bc_lat.lm<-lm(y_lat_boxed~as.matrix(xdat))
+reg_bc_long<-data.frame(ind=xdat, dep=y_long_boxed)
+reg_bc_long.lm<-lm(y_long_boxed~as.matrix(xdat))
+# 
+# Then I use the model to predict X into Y_predict. 
+predict_bc_lat<-predict(reg_bc_lat.lm,data.frame(xdat))
+predict_bc_long<-predict(reg_bc_long.lm,data.frame(xdat))
+
+# And finally I implemented an un-Box-Cox transformation, to transform Y_predict to Y_unboxed.
+un_bc_lat<-(predict_bc_lat*lambda_lat+1)^(1/lambda_lat)-90
+un_bc_long<-(predict_bc_long*lambda_long+1)^(1/lambda_long)-90
+# And calculate R2 using var(Y_unboxed)/var(Y_origin).
+bc_lat_r2 <- var(un_bc_lat)/var(y_lat_obj)
+bc_long_r2 <- var(un_bc_long)/var(y_long_obj)
