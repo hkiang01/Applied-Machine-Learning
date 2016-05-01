bd1f750cca8ceda840e763aa4d37380a13abae90
hkiang01 <hkiang01@yahoo.com>
2016-05-01 03:23:01 -0500

    2 conv layers to 4 conv layers

--- assignment8/cifar10_eval.py
+++ assignment8/cifar10_eval.py
@@ -122,7 +122,7 @@ def evaluate():
 
     # Build a Graph that computes the logits predictions from the
     # inference model.
-    logits = cifar10.inference(images)
+    logits = cifar10.inference3(images)
 
     # Calculate predictions.
     top_k_op = tf.nn.in_top_k(logits, labels, 1)
